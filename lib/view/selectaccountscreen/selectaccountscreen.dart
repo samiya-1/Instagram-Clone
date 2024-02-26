5169ab9ed1f1577687454b5d53b034d4bb3be365
sameea <opsameea@gmail.com>
2024-02-26 13:07:35 +0530

    first commit

---
+++ lib/view/selectaccountscreen/selectaccountscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelectAccountScreen extends StatefulWidget {
+  const SelectAccountScreen({super.key});
+
+  @override
+  State<SelectAccountScreen> createState() => _SelectAccountScreenState();
+}
+
+class _SelectAccountScreenState extends State<SelectAccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
